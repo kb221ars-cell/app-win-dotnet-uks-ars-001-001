7e4fa17805a276dd46452800d23a2c73d2145a6a
Adamchuk Rostyslav <kb221_ars@student.ztu.edu.ua>
2025-11-08 18:13:38 +0000

    add initial .NET app

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace app_win_dotnet_uks_ars_001_001.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
